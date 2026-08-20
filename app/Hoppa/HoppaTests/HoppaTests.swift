d173d978a92b821136065dca83e2ef9d2bbbf94b
robvb <hi@robvb.com>
2026-08-20 10:23:07 +0200

    Add the Xcode project files

---
+++ app/Hoppa/HoppaTests/HoppaTests.swift
@@ -0,0 +1,19 @@
+//
+//  HoppaTests.swift
+//  HoppaTests
+//
+//  Created by Rob on 20/08/2026.
+//
+
+import Testing
+@testable import Hoppa
+
+struct HoppaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
